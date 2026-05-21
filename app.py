fb9efc103085001e1930274ca3a9bfb2e694b3c6
harshugowda <hharshitha2001@gmail.com>
2026-05-21 15:11:51 +0000

    Added GitHub Actions CI/CD pipeline

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 import os
 import socket
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
+    with open("/app/logs/access.log", "a") as f:
+        f.write(f"Accessed at {datetime.now()}\n")
     return {
         "message": "Multi-Cloud DevOps Platform Running",
         "hostname": socket.gethostname(),
