12bba813c1a50111ecb78cbacec0a5bf64c0640a
harshugowda <hharshitha2001@gmail.com>
2026-05-24 11:55:43 +0000

    Testing automated deployment

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def home():
     with open("/app/logs/access.log", "a") as f:
         f.write(f"Accessed at {datetime.now()}\n")
     return {
-        "message": "Multi-Cloud DevOps Platform Running",
+        "message": "message": "CI/CD Pipeline Working Successfully",
         "hostname": socket.gethostname(),
         "environment": os.getenv("ENVIRONMENT", "development")
     }
